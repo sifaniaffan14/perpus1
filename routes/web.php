35af6cd27000524b14fd335c6ee508a535b111bc
bagus-masudi <ahmadbagusmasudi@gmail.com>
2023-06-19 06:30:59 +0700

    update style & add data in dashboard anggota

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ use App\Http\Controllers\InformasiPentingController;
 use App\Http\Controllers\Anggota\PencarianBukuController;
 use App\Http\Controllers\Anggota\CekPinjamanController;
 use App\Http\Controllers\Anggota\AnggotaPerpanjanganController;
+use App\Http\Controllers\Anggota\DashboardController as DashboardAnggota;
 use App\Http\Middleware\loginCheck;
 use App\Models\PeminjamanDetail;
 use Illuminate\Support\Facades\Auth;
@@ -188,7 +189,13 @@ Route::middleware('protectedPage:1')->group(function () {
 });
 
 Route::middleware('protectedPage:2')->group(function () {
-    Route::get('/dashboard',[DashboardController::class, 'anggota'])->name('dashboard');
+    Route::get('/dashboard',[DashboardAnggota::class, 'index'])->name('dashboard');
+    Route::controller(DashboardAnggota::class)->name('dashboard.')->prefix('dashboard')->group(function () {
+        $route = array('selectAnggota', 'selectInformasi', 'selectCount', 'selectPeminjaman');  
+        foreach ($route as $route) {
+            Route::any($route=='index'?'':'/'.$route, $route)->name($route);
+        }
+    });
 
     Route::controller(PencarianBukuController::class)->name('cariBuku.')->prefix('cariBuku')->group(function () {
         $route = array('index');  
