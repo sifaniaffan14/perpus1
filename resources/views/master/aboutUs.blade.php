12b9f05aa6e9b791f6815963779c5964dc177aa2
affansifani <affan.sekawanmedia@gmail.com>
2023-08-09 16:30:49 +0700

    aboutUs page

--- resources/views/master/aboutUs.blade.php
+++ resources/views/master/aboutUs.blade.php
@@ -9,15 +9,16 @@
         </section>      
         <section id="search__result" class="container card-about rounded rounded-4 overflow-auto mb-5">
             <div class="row">
-                <div class="col-lg-5 d-flex justify-content-center h-auto py-lg-5 pb-0" style="min-height: 300px">
+                <div class="col-lg-5 d-flex justify-content-center h-auto py-lg-5 pb-0 rounded" style="min-height: 300px">
                     <img src="images/perpustakaan2.png" class="mt-4" alt="" style="width:40vh; height:55vh">
                 </div>
-                <div class="col-lg-7 col-6 py-lg-5">
-                    <div class="pt-4 pb-2 header_content">
-                        <p>History</p>
+                <div class="col-lg-6 col-6 py-lg-5">
+                    <div class="pt-4 pb-2 header_about">
+                        <p>Profile</p>
                     </div>
-                    <div class="pt-2 pb-4 isi_content">
-                        <p>edwadwadwa</p>
+                    <div class="pt-2 pb-4 isi_about">
+                        <p>Perpustakaan SMP Al-Falah Ketintang Surabaya merupakan salah satu perpustakaan tingkat SMP yang ada di Kota Surabaya. Pengunjung dapat merasakan suasana perpustakaan yang nyaman dan lengkap dalam koleksi bukunya.</p>
+                        <p>Seiring dengan perjalanan sejarah sekolah, teriring pula perjalanan perpustakaan sekolah. Perpustakaan sekolah secara resmi berdiri sejak tahun 2010. Fasilitas Perpustakaan yang dimiliki Al-Falah Ketintang Surabaya berada di lantai satu bagian Timur belakang di kompleks SMP Al-Falah Ketintang Surabaya.</p>
                     </div>
                 </div>
             </div>
